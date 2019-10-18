d7c7d73caf63a3f8dbc579cc865b3b7dc964875b
VladimirKozachenko <vladimir.kozachenko@catenamedia.com>
2019-10-18 14:55:11 +0300

    start work

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Models\BlogPost;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(BlogPost::class, function (Faker $faker) {
+    $title = $faker->sentence(rand(3,8), true);
+    $txt = $faker->realText((rand(1000,4000)));
+    $isPublished = rand(1,5) > 1;
+    $created_at = $faker->dateTimeBetween('-3 month','-2 month');
+    $data = [
+      'category_id'  => rand(1,11),
+      'user_id'      => (rand(1,5) == 5) ? 1 : 2,
+      'title'        => $title,
+      'slug'         => Str::slug($title),
+      'excerpt'      => $faker->text(rand(40,100)),
+      'content_raw'  => $txt,
+      'content_html' => $txt,
+      'is_published' => $isPublished,
+      'published_at' => $isPublished ? $faker->dateTimeBetween('-2 month','-1 days') : null,
+      'created_at'   =>  $created_at,
+      'updated_at'   =>  $created_at,
+    ];
+
+
+    return $data;
+});
